57fd5513bf65150a049a8e760b1791483ef8362d
Søren A. R. Kynde <srk@dfm.dk>
2023-06-12 11:01:36 +0200

    Stage validator.py
    Usage:
    from validator import validate
    validate('file.xml','schema.xsd')

---
+++ validator.py
@@ -0,0 +1,20 @@
+from lxml import etree
+from urllib.request import urlopen
+
+def validate(xml_path: str, xsd_path: str) -> bool:
+    if xsd_path[0:5]=="https":
+    #Note: etree.parse can not handle https, so we have to open the url with urlopen
+       with urlopen(xsd_path) as xsd_file:
+          xmlschema_doc = etree.parse(xsd_file)
+    else:
+       xmlschema_doc = etree.parse(xsd_path)
+
+
+    xmlschema = etree.XMLSchema(xmlschema_doc)
+
+    xml_doc = etree.parse(xml_path)
+    result = xmlschema.validate(xml_doc)
+    print(result)
+    
+    return xmlschema.error_log.filter_from_errors()
+
