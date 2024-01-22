a41dd8061c7ff4e03daea3aaac704f99bdfd7a6f
Søren A. R. Kynde <srk@dfm.dk>
2024-01-22 21:14:34 +0100

    separate columns into row elements

--- DCChelpfunctions.py
+++ DCChelpfunctions.py
@@ -73,13 +73,13 @@ def validate(xml_path: str, xsd_path: str) -> bool:
     if xsd_path[0:5]=="https":
     #Note: etree.parse can not handle https, so we have to open the url with urlopen
        with urlopen(xsd_path) as xsd_file:
-          xmlschema_doc = etree.parse(xsd_file)
+          xmlschema_doc = et.parse(xsd_file)
     else:
-       xmlschema_doc = etree.parse(xsd_path)
+       xmlschema_doc = et.parse(xsd_path)
 
-    xmlschema = etree.XMLSchema(xmlschema_doc)
+    xmlschema = et.XMLSchema(xmlschema_doc)
 
-    xml_doc = etree.parse(xml_path)
+    xml_doc = et.parse(xml_path)
     result = xmlschema.validate(xml_doc)
     print(result)
 
